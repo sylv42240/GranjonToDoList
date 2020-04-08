6032e3b1341e43ab458ed59c0508974c3f9d6599
sylv42240 <sylv42240@gmail.com>
2020-04-08 09:07:10 +0200

    Project initialisation

---
+++ app/src/main/java/com/sylv42240/granjontodolist/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sylv42240.granjontodolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
